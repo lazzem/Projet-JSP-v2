c28603338fcb2ead4b524d69a72f8ce1f6e14f61
MalekBenJemaa <47210002+MalekBenJemaa@users.noreply.github.com>
2019-12-07 11:28:34 +0100

    ClaimChanges
    //ClaimChanges

--- ProjJSF2-ejb/src/main/java/model/PacksProduit.java
+++ ProjJSF2-ejb/src/main/java/model/PacksProduit.java
@@ -1,57 +1,57 @@
-package model;
-
-import java.io.Serializable;
-import javax.persistence.*;
-
-
-/**
- * The persistent class for the PacksProduits database table.
- * 
- */
-@Entity
-@Table(name="PacksProduits")
-@NamedQuery(name="PacksProduit.findAll", query="SELECT p FROM PacksProduit p")
-public class PacksProduit implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	@EmbeddedId
-	private PacksProduitPK id;
-
-	//bi-directional many-to-one association to Pack
-	@ManyToOne
-	@JoinColumn(name="Packs_IdPack")
-	private Pack pack;
-
-	//bi-directional many-to-one association to Produit
-	@ManyToOne
-	@JoinColumn(name="Produit_Id_produit")
-	private Produit produit;
-
-	public PacksProduit() {
-	}
-
-	public PacksProduitPK getId() {
-		return this.id;
-	}
-
-	public void setId(PacksProduitPK id) {
-		this.id = id;
-	}
-
-	public Pack getPack() {
-		return this.pack;
-	}
-
-	public void setPack(Pack pack) {
-		this.pack = pack;
-	}
-
-	public Produit getProduit() {
-		return this.produit;
-	}
-
-	public void setProduit(Produit produit) {
-		this.produit = produit;
-	}
-
-}
+//package model;
+//
+//import java.io.Serializable;
+//import javax.persistence.*;
+//
+//
+///**
+// * The persistent class for the PacksProduits database table.
+// * 
+// */
+//@Entity
+//@Table(name="PacksProduits")
+//@NamedQuery(name="PacksProduit.findAll", query="SELECT p FROM PacksProduit p")
+//public class PacksProduit implements Serializable {
+//	private static final long serialVersionUID = 1L;
+//
+//	@EmbeddedId
+//	private PacksProduitPK id;
+//
+//	//bi-directional many-to-one association to Pack
+//	@ManyToOne
+//	@JoinColumn(name="Packs_IdPack")
+//	private Pack pack;
+//
+//	//bi-directional many-to-one association to Produit
+//	@ManyToOne
+//	@JoinColumn(name="Produit_Id_produit")
+//	private Produit produit;
+//
+//	public PacksProduit() {
+//	}
+//
+//	public PacksProduitPK getId() {
+//		return this.id;
+//	}
+//
+//	public void setId(PacksProduitPK id) {
+//		this.id = id;
+//	}
+//
+//	public Pack getPack() {
+//		return this.pack;
+//	}
+//
+//	public void setPack(Pack pack) {
+//		this.pack = pack;
+//	}
+//
+//	public Produit getProduit() {
+//		return this.produit;
+//	}
+//
+//	public void setProduit(Produit produit) {
+//		this.produit = produit;
+//	}
+//
+//}
--- ProjJSF2-ejb/src/main/java/model/PacksProduitPK.java
+++ ProjJSF2-ejb/src/main/java/model/PacksProduitPK.java
@@ -1,57 +1,57 @@
-package model;
-
-import java.io.Serializable;
-import javax.persistence.*;
-
-/**
- * The primary key class for the PacksProduits database table.
- * 
- */
-@Embeddable
-public class PacksProduitPK implements Serializable {
-	//default serial version id, required for serializable classes.
-	private static final long serialVersionUID = 1L;
-
-	@Column(name="Packs_IdPack", insertable=false, updatable=false)
-	private int packs_IdPack;
-
-	@Column(name="Produit_Id_produit", insertable=false, updatable=false)
-	private int produit_Id_produit;
-
-	public PacksProduitPK() {
-	}
-	public int getPacks_IdPack() {
-		return this.packs_IdPack;
-	}
-	public void setPacks_IdPack(int packs_IdPack) {
-		this.packs_IdPack = packs_IdPack;
-	}
-	public int getProduit_Id_produit() {
-		return this.produit_Id_produit;
-	}
-	public void setProduit_Id_produit(int produit_Id_produit) {
-		this.produit_Id_produit = produit_Id_produit;
-	}
-
-	public boolean equals(Object other) {
-		if (this == other) {
-			return true;
-		}
-		if (!(other instanceof PacksProduitPK)) {
-			return false;
-		}
-		PacksProduitPK castOther = (PacksProduitPK)other;
-		return 
-			(this.packs_IdPack == castOther.packs_IdPack)
-			&& (this.produit_Id_produit == castOther.produit_Id_produit);
-	}
-
-	public int hashCode() {
-		final int prime = 31;
-		int hash = 17;
-		hash = hash * prime + this.packs_IdPack;
-		hash = hash * prime + this.produit_Id_produit;
-		
-		return hash;
-	}
-}
+//package model;
+//
+//import java.io.Serializable;
+//import javax.persistence.*;
+//
+///**
+// * The primary key class for the PacksProduits database table.
+// * 
+// */
+//@Embeddable
+//public class PacksProduitPK implements Serializable {
+//	//default serial version id, required for serializable classes.
+//	private static final long serialVersionUID = 1L;
+//
+//	@Column(name="Packs_IdPack", insertable=false, updatable=false)
+//	private int packs_IdPack;
+//
+//	@Column(name="Produit_Id_produit", insertable=false, updatable=false)
+//	private int produit_Id_produit;
+//
+//	public PacksProduitPK() {
+//	}
+//	public int getPacks_IdPack() {
+//		return this.packs_IdPack;
+//	}
+//	public void setPacks_IdPack(int packs_IdPack) {
+//		this.packs_IdPack = packs_IdPack;
+//	}
+//	public int getProduit_Id_produit() {
+//		return this.produit_Id_produit;
+//	}
+//	public void setProduit_Id_produit(int produit_Id_produit) {
+//		this.produit_Id_produit = produit_Id_produit;
+//	}
+//
+//	public boolean equals(Object other) {
+//		if (this == other) {
+//			return true;
+//		}
+//		if (!(other instanceof PacksProduitPK)) {
+//			return false;
+//		}
+//		PacksProduitPK castOther = (PacksProduitPK)other;
+//		return 
+//			(this.packs_IdPack == castOther.packs_IdPack)
+//			&& (this.produit_Id_produit == castOther.produit_Id_produit);
+//	}
+//
+//	public int hashCode() {
+//		final int prime = 31;
+//		int hash = 17;
+//		hash = hash * prime + this.packs_IdPack;
+//		hash = hash * prime + this.produit_Id_produit;
+//		
+//		return hash;
+//	}
+//}
